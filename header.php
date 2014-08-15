d6c9ecc050c4ce0204341efb4e623b28898c7291
Taylor Lovett <admin@taylorlovett.com>
2014-08-14 23:45:19 -0400

    Escaping for bloginfo

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php esc_html( bloginfo( 'name' ) ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
 
