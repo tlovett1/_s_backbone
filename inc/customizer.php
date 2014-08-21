b3e3d0c5ac288cd64b71b3e708937872117353cd
brandwaffle <vhauri@gmail.com>
2014-08-21 05:19:58 +0000

    cleaned up a couple more misnamed functions from find/replace

--- inc/customizer.php
+++ inc/customizer.php
@@ -21,6 +21,6 @@ add_action( 'customize_register', '_s_backbone_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function _s_backbone_customize_preview_js() {
-	wp_enqueue_s_backbonecript( '_s_backbone_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+	wp_enqueue_script( '_s_backbone_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
 }
 add_action( 'customize_preview_init', '_s_backbone_customize_preview_js' );
